2a3d594552ba3f5f73bb01c2c92b1d9c76d13738
Augs Machado <augustomachadoramos@gmail.com>
2022-03-17 21:09:51 -0300

    routes simplification

--- index.js
+++ index.js
@@ -10,10 +10,10 @@ const PORT = process.env.PORT || 5000;
 
 app.use(express.json());
 
-app.use("/api/status", status);
-app.use("/api/v1/circulation", circulation);
-app.use("/api/v1/vet", vet);
-app.use("/api/v1/ccr", ccr);
+app.use("/", status);
+app.use("/circulation", circulation);
+app.use("/vet", vet);
+app.use("/ccr", ccr);
 
 app.use("*", (req, res) => {
 	res.status(400).json({ error: "Not route found" });
